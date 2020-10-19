7a795ea916848a66520e534e02d18d73ee487a38
Nicola Spanu <nicspanu@gmail.com>
2020-10-19 11:00:55 +0200

    second commit

--- lists/views.py
+++ lists/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-home_page = None
+def home_page(request):
+    return HttpResponse('<html><title>To-Do lists</title></html>')
