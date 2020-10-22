9f12ac05b549cad96753465947a98e30bbbd88b4
Nicola Spanu <nicspanu@gmail.com>
2020-10-22 14:30:38 +0200

    ninth commit

--- lists/views.py
+++ lists/views.py
@@ -3,11 +3,12 @@ from lists.models import Item
 from django.http import HttpResponse
 
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
     return render(request, 'home.html')
 
 def view_list(request):
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
+
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
