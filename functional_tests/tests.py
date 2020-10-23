a91ad3c531e26fa0d5aa5d80a802764be89b3149
Nicola Spanu <nicspanu@gmail.com>
2020-10-23 13:03:03 +0200

    eleventh commit

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -111,13 +111,4 @@ class NewVisitorTest(LiveServerTestCase):
 
         # Satisfied, they both go back to sleep
 
-        # Edith wonders whether the site will remember her list. Then she sees
-        # that the site has generated a unique URL for her -- there is some
-        # explanatory text to that effect.
-        self.fail('Finish the test!')
-
-        # She visits that URL - her to-do list is still there.
-
-        # Satisfied, she goes back to sleep
-
 
